121d76495dcf43e86d4f12f34564b819c1377b33
pratyakshpandey <pratyaksh.pandey@worldfashionexchange.com>
2025-08-10 13:23:37 +0530

    fix: calender

--- src/components/Calender/Calender.tsx
+++ src/components/Calender/Calender.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./calender.css";
+import "./Calender.css";
 import axios from "axios";
 import { BASE_URL } from "../../enviroment/enviroment";
 
